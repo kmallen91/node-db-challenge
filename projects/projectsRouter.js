0941b0ba031ff424bef61451969782cc94b89575
Kevin Allen <kevin.allen91@gmail.com>
2019-11-08 08:35:04 -0800

    initialized project, downloaded deps

---
+++ projects/projectsRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const projects = require("./projectsModel");
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/", (req, res) => {});
+
+router.delete("/", (req, res) => {});
